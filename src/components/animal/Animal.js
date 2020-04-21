9dba05f8c79d3d1d56fb0b0ac58f967572b84271
robdan89 <fuquardwork@gmail.com>
2020-04-21 14:39:01 -0500

    replaced the location ID being disployed with the employees to a location name.

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -4,7 +4,7 @@ export default ({ animal, customer, location }) => (
   <section className="animal">
     <h3 className="animal__name">{animal.name}</h3>
     <div className="animal__breed">{animal.breed}</div>
-    <div className="animal__breed">Location: {location.name}</div>
-    <div className="animal__breed">Customer: {customer.name}</div>
+    <div className="animal_location">Location: {location.name}</div>
+    <div className="animal__owner">Customer: {customer.name}</div>
   </section>
 );
