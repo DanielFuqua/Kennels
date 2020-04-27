a3b66dd3efce841e19d4c5f84d816c22ece3496d
robdan89 <fuquardwork@gmail.com>
2020-04-27 14:42:35 -0500

    can now edit animals.

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -2,9 +2,8 @@ import React from "react";
 
 export default ({ animal, customer, location }) => (
   <section className="animal">
-    <h3 className="animal__name">{animal.name}</h3>
     <div className="animal__breed">{animal.breed}</div>
-    <div className="animal_location">Location: {location.name}</div>
-    <div className="animal__owner">Customer: {customer.name}</div>
+    <div className="animal_location">Checked-in at: {location.name}</div>
+    <div className="animal__owner">Owner: {customer.name}</div>
   </section>
 );
--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AnimalContext } from "../animal/AnimalProvider";
-import { Modal, ModalHeader, ModalBody, Button } from "reactstrap";
+import { Modal, ModalHeader, ModalBody, Button, ModalFooter } from "reactstrap";
 import { CustomerContext } from "../customer/CustomerProvider";
 import { LocationContext } from "../location/LocationProvider";
 import Animal from "../animal/Animal";
+import { EditAnimalForm } from "../animal/EditAnimalForm";
 
 export const SearchResults = ({ searchTerms }) => {
   const { animals, releaseAnimal } = useContext(AnimalContext);
@@ -17,8 +18,16 @@ export const SearchResults = ({ searchTerms }) => {
     customer: null,
   });
 
+  // Toggle details modal for other animals (no edit/delete buttons)
+  const [modal2, setModal2] = useState(false);
+  const toggle2 = () => setModal2(!modal2);
+  // Toggle details modal for your animal (with edit/delete buttons)
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
+
+  // Toggle edit modal
+  const [editModal, setEditModal] = useState(false);
+  const toggleEdit = () => setEditModal(!editModal);
 
   useEffect(() => {
     if (searchTerms !== "") {
@@ -39,36 +48,81 @@ export const SearchResults = ({ searchTerms }) => {
           <div
             className="fakeLink href"
             key={animal.id}
-            onClick={() => {
-              const location = locations.find(
-                (l) => l.id === animal.locationId
-              );
-              const customer = customers.find(
-                (c) => c.id === animal.customerId
-              );
-
-              setAnimal({ animal, location, customer });
-              toggle();
-            }}
+            onClick={
+              animal.customerId ===
+              parseInt(localStorage.getItem("kennel_customer"))
+                ? () => {
+                    const location = locations.find(
+                      (l) => l.id === animal.locationId
+                    );
+                    const customer = customers.find(
+                      (c) => c.id === animal.customerId
+                    );
+                    setAnimal({ animal, location, customer });
+                    toggle();
+                  }
+                : () => {
+                    const location = locations.find(
+                      (l) => l.id === animal.locationId
+                    );
+                    const customer = customers.find(
+                      (c) => c.id === animal.customerId
+                    );
+                    setAnimal({ animal, location, customer });
+                    toggle2();
+                  }
+            }
           >
             {animal.name}
           </div>
         ))}
       </div>
+
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>{selectedAnimal.animal.name}</ModalHeader>
         <ModalBody>
           <Animal key={selectedAnimal.animal.id} {...selectedAnimal} />
         </ModalBody>
-        <Button
-          color="danger"
-          onClick={() => {
-            releaseAnimal(selectedAnimal.animal.id);
-            toggle();
-          }}
-        >
-          Delete
-        </Button>
+        <ModalFooter>
+          <Button
+            color="info"
+            onClick={() => {
+              toggle();
+              toggleEdit();
+            }}
+          >
+            Edit
+          </Button>
+          <Button
+            color="danger"
+            onClick={() => {
+              releaseAnimal(selectedAnimal.animal.id);
+              toggle();
+            }}
+          >
+            Delete
+          </Button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modal2} toggle={toggle2}>
+        <ModalHeader toggle={toggle2}>{selectedAnimal.animal.name}</ModalHeader>
+        <ModalBody>
+          <Animal key={selectedAnimal.animal.id} {...selectedAnimal} />
+        </ModalBody>
+      </Modal>
+
+      <Modal isOpen={editModal} toggle={toggleEdit}>
+        <ModalHeader toggle={toggleEdit}>
+          {selectedAnimal.animal.name}
+        </ModalHeader>
+        <ModalBody>
+          <EditAnimalForm
+            key={selectedAnimal.animal.id}
+            toggleEdit={toggleEdit}
+            {...selectedAnimal}
+          />
+        </ModalBody>
       </Modal>
     </div>
   );
